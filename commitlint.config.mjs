6e76a32a10f16f85bfda22a68737ee7174101518
Dana Natasha Cadabon <natashacadabon@gmail.com>
2026-09-22 18:47:49 -0300

    fix: corrige tooling local de Git

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,22 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'ci', 'test', 'docs']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'test',
+        'refactor',
+        'style',
+        'perf',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
   },
 };
